b1d39382b5eedd24b07ea5b3574fde10826491d5
divyapandit13 <divyapandit13@gmail.com>
2020-11-23 15:01:33 -0500

    Adding service

--- Webapp/src/main/java/org/studyeasy/controller/PostsController.java
+++ Webapp/src/main/java/org/studyeasy/controller/PostsController.java
@@ -6,25 +6,18 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.studyeasy.entity.Post;
+import org.studyeasy.services.PostsService;
 
 
 @RestController
 public class PostsController {
 
-	List<Post> posts = new ArrayList<>();
 	
-
-	public PostsController() {
-	posts.add(new Post(1,"hello", "world"));
-	posts.add(new Post(2,"lamda Expressions", "core java"));
-	posts.add(new Post(3,"OOP", "java"));
-	posts.add(new Post(4,"Inheritance", "Polymorphism"));
-	}
 	
 	
 	@RequestMapping("/posts")
 	public List<Post> getAllPosts(){
-		return posts;
+		return new PostsService().getAllPosts();
 	}
 	
 	
---
+++ Webapp/src/main/java/org/studyeasy/services/PostsService.java
@@ -0,0 +1,25 @@
+package org.studyeasy.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.studyeasy.entity.Post;
+
+
+@Service
+public class PostsService {
+List<Post> posts = new ArrayList<>();
+	
+
+	public PostsService() {
+	posts.add(new Post(1,"hello", "world"));
+	posts.add(new Post(2,"lamda Expressions", "core java"));
+	posts.add(new Post(3,"OOP", "java"));
+	posts.add(new Post(4,"Inheritance", "Polymorphism"));
+	}
+	
+	public List<Post> getAllPosts(){
+		return posts;
+	}
+}
