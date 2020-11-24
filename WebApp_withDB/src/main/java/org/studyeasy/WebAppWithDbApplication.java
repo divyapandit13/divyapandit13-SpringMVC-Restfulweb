4598f9480f6f1f79795933230a79bd36e0ddfc39
divyapandit13 <divyapandit13@gmail.com>
2020-11-24 14:33:38 -0500

    base model of web application with database

---
+++ WebApp_withDB/src/main/java/org/studyeasy/WebAppWithDbApplication.java
@@ -0,0 +1,13 @@
+package org.studyeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppWithDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppWithDbApplication.class, args);
+	}
+
+}
