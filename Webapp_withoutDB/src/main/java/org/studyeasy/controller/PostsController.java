fca1d32845763e40520e2f65ee2e98f050aa08b3
divyapandit13 <divyapandit13@gmail.com>
2020-11-24 14:41:21 -0500

    base model of web application without database

---
+++ Webapp_withoutDB/src/main/java/org/studyeasy/controller/PostsController.java
@@ -0,0 +1,55 @@
+package org.studyeasy.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.studyeasy.entity.Post;
+import org.studyeasy.services.PostsService;
+
+
+@RestController
+public class PostsController {
+
+	
+	
+	//get all posts
+	@RequestMapping("/posts")
+	public List<Post> getAllPosts(){
+		return new PostsService().getAllPosts();
+	}
+	
+	
+	//get particular post
+	@RequestMapping("/posts/{id}")
+	public Post getPost(@PathVariable int id){
+		return new PostsService().getPost(id);
+	}
+	
+	
+	//add a post
+		@RequestMapping(method = RequestMethod.POST, value = "/posts")
+		public void addPost(@RequestBody Post listElement){
+			 new PostsService().addPost(listElement);
+		}
+	
+		
+		//update a post
+				@RequestMapping(method = RequestMethod.PUT, value = "/posts/{id}")
+				public void updatePost(@RequestBody Post post,@PathVariable int id){
+					 new PostsService().updatePost(post,id);
+				}
+				
+				//delete a post
+				@RequestMapping(method = RequestMethod.DELETE, value = "/posts/{id}")
+				public void deletePost(@PathVariable int id){
+					 new PostsService().deletePost(id);
+				}
+				
+
+	
+	
+}
